df409507b43cc9a558a820c8fd64620ef8357567
AetherFire23 <not@specified@gmail.com>
2026-03-05 21:52:17 -0500

    tests really some to work with many prompts and screen

--- tests/ERP.Unit/Actions/GameActionsWithTargetsValidatorTest.cs
+++ tests/ERP.Unit/Actions/GameActionsWithTargetsValidatorTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using AetherFire23.ERP.Domain.Actions;
+﻿using AetherFire23.ERP.Domain.Actions;
 using AetherFire23.ERP.Domain.Actions.AvailabilityStuff;
 using JetBrains.Annotations;
 using NorthwestV2.Application.UseCases.GameActions.Queries.GetActions.ReturnValue;
@@ -13,25 +12,25 @@ public class GameActionsWithTargetsValidatorTest
     {
         TargetId = new Guid("6ab835b4-8b70-4e8e-97ec-937e6d012e5c"),
     };
-    
 
-    
+
     // == value prompts ==
 
     private static TargetSelectionPrompt VALUE_PROMPT = new TargetSelectionPrompt()
     {
         Description = nameof(VALUE_PROMPT),
-        ValidTargets = [new ActionTarget(){Value = "1"}]
-    };
-    
-    private static ActionWithTargetsAvailability AVAILABILY_ONE_PROMPT_VALUES_MIN_1_MAX_1 = new ActionWithTargetsAvailability
-    {
-        ActionName = "blabla",
-        ActionRequirements = [new ActionRequirement(){Description = "wrks", IsFulfilled = true}],
-        TargetSelectionPrompts = [VALUE_PROMPT]
+        ValidTargets = [new ActionTarget() { Value = "1" }]
     };
 
-    
+    private static ActionWithTargetsAvailability AVAILABILY_ONE_PROMPT_VALUES_MIN_1_MAX_1 =
+        new ActionWithTargetsAvailability
+        {
+            ActionName = "blabla",
+            ActionRequirements = [new ActionRequirement() { Description = "wrks", IsFulfilled = true }],
+            TargetSelectionPrompts = [VALUE_PROMPT]
+        };
+
+
     private static ActionTarget SAME_TARGET_WITH_VALUE_1 = new ActionTarget
     {
         Value = "1"
@@ -51,7 +50,7 @@ public class GameActionsWithTargetsValidatorTest
     {
         TargetId = new Guid("c98b8006-1a5e-4495-8655-b40897758549"),
     };
-    
+
     private static ActionTarget ANY_RANDOM_TARGET = new ActionTarget
     {
         TargetId = new Guid("7b2c9c78-08f1-4df9-be69-358c3da5f6b8"),
@@ -103,26 +102,31 @@ public class GameActionsWithTargetsValidatorTest
         Max = 2
     };
 
-    private static ActionWithTargetsAvailability AVAILABILITY_SINGLE_PROMPT_MIN_1_MAX_1_ = new ActionWithTargetsAvailability
-    {
-        ActionName = "blabla",
-        ActionRequirements = [FULFILLED_ACTION_REQUIREMENT],
-        TargetSelectionPrompts = [MIN1_MAX_1_TWO_TARGETS_PROMPT_1]
-    };
-    
+    private static ActionWithTargetsAvailability AVAILABILITY_SINGLE_PROMPT_MIN_1_MAX_1_ =
+        new ActionWithTargetsAvailability
+        {
+            ActionName = "blabla",
+            ActionRequirements = [FULFILLED_ACTION_REQUIREMENT],
+            TargetSelectionPrompts = [MIN1_MAX_1_TWO_TARGETS_PROMPT_1]
+        };
+
     private static ActionWithTargetsAvailability TWO_PROMPTS_MIN_1_MAX_1_ = new ActionWithTargetsAvailability
     {
         ActionName = "blabla",
         ActionRequirements = [FULFILLED_ACTION_REQUIREMENT],
         TargetSelectionPrompts = [MIN1_MAX_1_TWO_TARGETS_PROMPT_1, MIN1_MAX_1_PROMPT__2]
     };
-    
+
 
     private static List<List<ActionTarget>> ANY_ACTION_TARGETS = [[]];
     private static List<List<ActionTarget>> ONE_PROMPT_ONE_SELECTION_ACTION_TARGET = [[ANY_TARGET_WITH_ID_1]];
     private static List<List<ActionTarget>> ONE_PROMPT_ONE_SELECTION_RANDOM_ACTION_TARGET = [[ANY_RANDOM_TARGET]];
-    private static List<List<ActionTarget>> ONE_PROMPT_MANY_SELECTIONS_ACTION_TARGET = [[ANY_TARGET_WITH_ID_1, ANY_TARGET_WITH_ID_2]];
-    private static List<List<ActionTarget>> ONE_PROMPT_TOO_MANY_SELECTIONS_ACTION_TARGET = [[ANY_TARGET_WITH_ID_1, ANY_TARGET_WITH_ID_2, ANY_TARGET_WITH_ID_3, ANY_TARGET_WITH_ID_4]];
+
+    private static List<List<ActionTarget>> ONE_PROMPT_MANY_SELECTIONS_ACTION_TARGET =
+        [[ANY_TARGET_WITH_ID_1, ANY_TARGET_WITH_ID_2]];
+
+    private static List<List<ActionTarget>> ONE_PROMPT_TOO_MANY_SELECTIONS_ACTION_TARGET =
+        [[ANY_TARGET_WITH_ID_1, ANY_TARGET_WITH_ID_2, ANY_TARGET_WITH_ID_3, ANY_TARGET_WITH_ID_4]];
 
     private static List<List<ActionTarget>> TWO_PROMPT_ONE_SELECTION_ACTION_TARGET =
         [[ANY_TARGET_WITH_ID_1], [ANY_TARGET_WITH_ID_2]];
@@ -148,8 +152,6 @@ public class GameActionsWithTargetsValidatorTest
         gameActionsWithTargetsValidator.EnsureIsValidActionExecutionOrThrow(AVAILABILITY_SINGLE_PROMPT_MIN_1_MAX_1_,
             ONE_PROMPT_ONE_SELECTION_ACTION_TARGET);
     }
-    
-    
 
     [Fact]
     public void GivenTwoPrompts_WhenValidated_ThenDoesNotThrow()
@@ -159,19 +161,19 @@ public class GameActionsWithTargetsValidatorTest
         gameActionsWithTargetsValidator.EnsureIsValidActionExecutionOrThrow(TWO_PROMPTS_MIN_1_MAX_1_,
             TWO_PROMPT_ONE_SELECTION_ACTION_TARGET);
     }
-    
+
     [Fact]
     public void GivenOnePromptWithTooFewSelections_WhenValidated_ThenThrows()
     {
         GameActionsWithTargetsValidator gameActionsWithTargetsValidator = new GameActionsWithTargetsValidator();
 
         Action action = () =>
-        gameActionsWithTargetsValidator.EnsureIsValidActionExecutionOrThrow(AVAILABILITY_SINGLE_PROMPT_MIN_1_MAX_1_,
-            ONE_PROMPT_MANY_SELECTIONS_ACTION_TARGET);
+            gameActionsWithTargetsValidator.EnsureIsValidActionExecutionOrThrow(AVAILABILITY_SINGLE_PROMPT_MIN_1_MAX_1_,
+                ONE_PROMPT_MANY_SELECTIONS_ACTION_TARGET);
 
         Assert.Throws<Exception>(action);
     }
-    
+
     [Fact]
     public void GivenOnePromptWithTooManySelections_WhenValidated_ThenThrows()
     {
@@ -183,7 +185,7 @@ public class GameActionsWithTargetsValidatorTest
 
         Assert.Throws<Exception>(action);
     }
-    
+
     [Fact]
     public void GivenInvalidTargetProvided_WhenValidated_ThenThrows()
     {
@@ -195,9 +197,35 @@ public class GameActionsWithTargetsValidatorTest
 
         Assert.Throws<Exception>(action);
     }
+
+    // TODO: test min maxxess
+    [Fact]
+    public void GivenPromptWithLargeMinMaxBounds_WhenValidated_ThenDoesNotThrow()
+    {
+        GameActionsWithTargetsValidator gameActionsWithTargetsValidator = new GameActionsWithTargetsValidator();
+        var prompt = new TargetSelectionPrompt()
+        {
+            Description = "MIN2 MAX 2 TARGET SELECTION PROMPT",
+            ValidTargets = [ANY_TARGET_WITH_ID_1,ANY_TARGET_WITH_ID_2],
+            Min = 2,
+            Max = 2
+        };
+        ActionWithTargetsAvailability availability = new ActionWithTargetsAvailability
+        {
+            ActionName = "blabla",
+            ActionRequirements = [FULFILLED_ACTION_REQUIREMENT],
+            TargetSelectionPrompts = [prompt]
+        };
+        List<List<ActionTarget>> actionsTargets = [[ANY_TARGET_WITH_ID_1, ANY_TARGET_WITH_ID_2]];
+
     
+            gameActionsWithTargetsValidator.EnsureIsValidActionExecutionOrThrow(availability, actionsTargets);
+
+    }
+
+
     // === VALUE SELECTIONS ==
-    
+
     [Fact]
     public void GivenSinglePromptWithValueTargets_WhenValidated_ThenDoesNotThrow()
     {
@@ -206,6 +234,4 @@ public class GameActionsWithTargetsValidatorTest
         gameActionsWithTargetsValidator.EnsureIsValidActionExecutionOrThrow(AVAILABILY_ONE_PROMPT_VALUES_MIN_1_MAX_1,
             [[SAME_TARGET_WITH_VALUE_1]]);
     }
-
-    // TODO: test to check if comparisons still work with values instead of ids
 }
