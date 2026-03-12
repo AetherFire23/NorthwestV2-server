03eeb8b968ec412533041ae6961ac5249834ee7d
AetherFire23 <not@specified@gmail.com>
2026-03-12 02:25:37 -0400

    comp

---
+++ NorthwestV2.Compose/AppComposer.cs
@@ -0,0 +1,27 @@
+﻿using AetherFire23.ERP.Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NorthwestV2.Application.Installation;
+using NorthwestV2.Infrastructure.Contexts;
+
+namespace NorthwestV2.Compose;
+
+public static class AppComposer
+{
+    public static void ComposeApplication(IServiceCollection serviceCOllection, IConfiguration configuration)
+    {
+        NorthwestContextInstaller.Install(serviceCOllection, configuration);
+        
+        InfraInstaller.Install(serviceCOllection);
+        MediatorInstaller.Install(serviceCOllection);
+        //
+        // //
+        DomainInstaller.Install(serviceCOllection, configuration);
+        ApplicationInstaller.Install(serviceCOllection);
+    }
+
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        NorthwestContextInstaller.Initialize(serviceProvider);
+    }
+}
