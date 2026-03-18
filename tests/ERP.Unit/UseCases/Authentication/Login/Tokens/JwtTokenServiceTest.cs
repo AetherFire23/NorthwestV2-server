ae0340362e3e5b9fa301bfc4cb54492ff75d0661
AetherFire23 <not@specified@gmail.com>
2026-03-18 08:51:46 -0400

    Added jwt

---
+++ tests/ERP.Unit/UseCases/Authentication/Login/Tokens/JwtTokenServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using JetBrains.Annotations;
+using NorthwestV2.Application.UseCases.Authentication.Login.Tokens;
+
+namespace ERP.Testing.Domain.UseCases.Authentication.Login.Tokens;
+
+[TestSubject(typeof(JwtTokenService))]
+public class JwtTokenServiceTest
+{
+    [Fact]
+    public void GivenJwTokenServce_canIssue_token()
+    {
+        var jwtManager = new JwtTokenService();
+
+
+        string token = jwtManager.GenerateToken(Guid.NewGuid());
+        
+        Assert.True(token != null);
+    }
+    
+    [Fact]
+    public void GivenJwTokenServce_canValidateToken_Corecctly()
+    {
+        var jwtManager = new JwtTokenService();
+
+        string token = jwtManager.GenerateToken(Guid.NewGuid());
+
+        var isValid = jwtManager.ValidateToken(token);
+
+        Assert.NotNull(isValid);
+    }
+}
