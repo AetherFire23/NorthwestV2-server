2a3a7b15fd3c48f56bd639239229455b4b386401
Frederik Richer Balthazard <2410658@cegeplimoilou.ca>
2026-04-20 13:50:09 -0400

    swapitemownership

---
+++ tests/ERP.Integration/UseCases/Items/Commands/SwapItemOwnershipHandlerTest.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using NorthwestV2.Features.UseCases.Items.Commands;
+using NorthwestV2.Integration.Scratches;
+using Xunit.Abstractions;
+
+namespace NorthwestV2.Integration.UseCases.Items.Commands;
+
+[TestSubject(typeof(SwapItemOwnershipHandler))]
+public class SwapItemOwnershipHandlerTest : TestBase2
+{
+    public SwapItemOwnershipHandlerTest(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public void GivenItemInRoom_When_RyanThen()
+    {
+        
+        Assert.Fail();
+    }
+}
